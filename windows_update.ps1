0c1c88cd8f4ece28e21d5e86ddd22f466407b01e
Louisa Gracia Diego <lgadiego001@outlook.com>
2025-12-17 14:35:37 +0800

    winget

---
+++ windows_update.ps1
@@ -0,0 +1,12 @@
+# Run in PowerShell as Administrator
+#Install-Module PSWindowsUpdate -Force
+Update-Module PSWindowsUpdate
+
+# Import the module
+Import-Module PSWindowsUpdate
+
+# Check for updates
+Get-WindowsUpdate
+
+# Install updates
+Install-WindowsUpdate -AcceptAll -AutoReboot
